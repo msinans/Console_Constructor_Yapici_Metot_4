144e1616e27ff1202f225d2b0ff77db2b7031a8e
m.sinans <msomercikoglu@gmail.com>
2023-03-14 14:14:22 +0300

    Add project files.

---
+++ Console_Constructor_Yapici_Metot_4/Kimlik.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Constructor_Yapici_Metot_4
+{
+    internal class Kimlik
+    {
+        // değişken isimlerini atıyoruz
+        string ad;
+        string soyad;
+        string sehir;
+        int yas;
+        char cinsiyet;
+
+        // Kapsülleme kullanıyoruz
+        public string AD
+        {
+            get { return ad; }
+            set { ad = value.ToLower(); }
+        }
+        public string SOYAD
+        {
+            get { return soyad; }
+            set { soyad = value.ToUpper(); }
+        }
+        public string SEHİR
+        {
+            get { return sehir; }
+            set { sehir = value; }
+        }
+
+        public int YAS
+        {
+            get { return yas; }
+            set { yas = Math.Abs(value); }
+        }
+        public char CINSIYET
+        {
+            get { return cinsiyet; }
+            set { cinsiyet = value; }
+        }
+        // Constructor kullanarak ilk değer ataması yapıyoruz
+        public Kimlik()
+        {
+            ad = "";
+            soyad = "";
+            sehir = "İstanbul";
+            yas = 18;
+            cinsiyet = 'K';
+        }
+    }
+}
---
+++ Console_Constructor_Yapici_Metot_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Constructor_Yapici_Metot_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kimlik kml = new Kimlik();
+            kml.AD = "ALİ";
+            kml.SOYAD = "ömercikoğlu";
+            kml.CINSIYET = 'E'; // Sınıf içerisinde ilk değer ataması olsa da artık bunu geçerli kabul ediyor
+            Console.WriteLine(kml.AD);
+            Console.WriteLine(kml.SOYAD);
+            Console.WriteLine(kml.YAS);
+            Console.WriteLine(kml.CINSIYET);
+            Console.WriteLine(kml.SEHİR);
+
+            Console.Read();
+        }
+    }
+}
